9cc87b1b5cb0d4e863f1147c1750a0b4a4007b04
YonatanG <yonatangr86@gmail.com>
2021-06-24 19:06:18 +0300

    server update with DB

--- main/currency/view.py
+++ main/currency/view.py
@@ -1,10 +1,17 @@
 from flask import Blueprint
-# from model.message_model import Message
+import requests
+import json
+from ..currency.message_model import Currency
 # from utilities.utilities import login_required
 
 currency = Blueprint('currency', __name__)
 
 
-@currency.route('/', methods=['POST'])
+@currency.route('/', methods=['GET'])
 def Hello():
-    return 'Hello'
+    req = requests.get(
+        'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/ils.json')
+    currency_update = json.loads(req.content)
+    print(currency_update)
+    Currency().update(currency_update)
+    return 'Hello, you reach currency update server'
